8510de4f34127544f5032a2304d86a2cfbc2497c
ST3607 <>
2021-12-04 14:23:14 +0000

    Files

--- src/exercise7/Triangle.java
+++ src/exercise7/Triangle.java
@@ -2,28 +2,28 @@ package exercise7;
 
 import java.util.Scanner;
 
-public class Triangle {
-    public static void main(String[] args) {
+public class Triangle{
+    public static void main(String[] args){
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter n: ");
         int n = input.nextInt();
 
-        for (int line = 1; line <= n; line++) {
+        for (int line = 1; line <= n; line++){
             printLine(1, line);
         }
 
-        for (int line = n - 1; line >= 1; line--) {
+        for (int line = n - 1; line >= 1; line--){
           printLine(1, line);
         }
         // Print the bottom part of the triangle that is under the longest line   
     }
-    public static void printLine(int start, int end) {
-      for (int number = start; number <= end; number++) {
+    public static void printLine(int start, int end){
+      for (int number = start; number <= end; number++){
         System.out.print(number);
-        if(number != end) {
+        if(number != end){
          System.out.print(" ");
-        } else {
+        }else{
           System.out.print(" ");
         }
       }
