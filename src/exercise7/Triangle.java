70e4949cce1bbb12d75f08015f246c6cacddcdbb
ST3607 <>
2021-12-04 15:42:52 +0000

    Files

--- src/exercise7/Triangle.java
+++ src/exercise7/Triangle.java
@@ -24,9 +24,9 @@ public class Triangle {
     for(int number = start; number <= end; number++) {
       System.out.print(number);
       if (number !=end) {
-        System.out.print(" ");
+        System.out.print("");
       } else {
-        System.out.print(" ");
+        System.out.print("");
       }
     }
     System.out.println();
